726bd55be929e3a76b01cc7cc948cea281d0a1f4
David Brázdil <david.brazdil@gmail.com>
2012-10-19 17:12:49 +0100

    Maven project

---
+++ src/main/java/uk/ac/cam/db538/dexter/App.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.db538.dexter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
