95128237b3f45660c5b62523cb5cb5a81905c260
David Brázdil <david.brazdil@gmail.com>
2012-10-19 21:01:39 +0100

    MainWindow

--- src/uk/ac/cam/db538/dexter/App.java
+++ src/uk/ac/cam/db538/dexter/App.java
@@ -1,16 +1,10 @@
 package uk.ac.cam.db538.dexter;
 
+import uk.ac.cam.db538.dexter.gui.MainWindow;
 import lombok.val;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	val number = new Integer(42);
-        System.out.println( "Hello World! " + number.toString() );
+public class App {
+	
+    public static void main( String[] args ) {
     }
 }
---
+++ src/uk/ac/cam/db538/dexter/gui/MainWindow.java
@@ -0,0 +1,48 @@
+package uk.ac.cam.db538.dexter.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JSplitPane splitPane = new JSplitPane();
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+	}
+
+}
