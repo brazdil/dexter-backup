c38597d20f32133d611b51731d1acbadbba50b7b
David Brazdil <david.brazdil@gmail.com>
2012-12-11 00:22:09 +0100

    Removed unused code

--- test/uk/ac/cam/db538/dexter/dex/code/insn/DexInstruction_MoveException_Test.java
+++ test/uk/ac/cam/db538/dexter/dex/code/insn/DexInstruction_MoveException_Test.java
@@ -46,32 +46,4 @@ public class DexInstruction_MoveException_Test {
 
     insn.assembleBytecode(Utils.genAsmState(regAlloc));
   }
-
-//  @Test
-//  public void testAssemble_Object() {
-//    val rTo = Utils.numFitsInto_Unsigned(8);
-//    val regTo = new DexRegister(rTo);
-//    val regAlloc = Utils.genRegAlloc(regTo);
-//
-//    val insn = new DexInstruction_MoveResult(null, regTo, true);
-//
-//    val asm = insn.assembleBytecode(Utils.genAsmState(regAlloc));
-//    assertEquals(1, asm.length);
-//    assertTrue(asm[0] instanceof Instruction11x);
-//
-//    val asmInsn = (Instruction11x) asm[0];
-//    assertEquals(Opcode.MOVE_RESULT_OBJECT, asmInsn.opcode);
-//    assertEquals(rTo, asmInsn.getRegisterA());
-//  }
-//
-//  @Test(expected=InstructionAssemblyException.class)
-//  public void testAssemble_WrongAllocation() {
-//    val rTo = Utils.numFitsInto_Unsigned(9);
-//    val regTo = new DexRegister(rTo);
-//    val regAlloc = Utils.genRegAlloc(regTo);
-//
-//    val insn = new DexInstruction_MoveResult(null, regTo, true);
-//
-//    insn.assembleBytecode(Utils.genAsmState(regAlloc));
-//  }
 }
