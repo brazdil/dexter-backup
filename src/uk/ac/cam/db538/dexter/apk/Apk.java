f610360d2875af35330a217ff5185b7a93b57a0e
David Brazdil <david.brazdil@gmail.com>
2013-01-16 23:30:27 +0000

    Signes APKs

--- src/uk/ac/cam/db538/dexter/apk/Apk.java
+++ src/uk/ac/cam/db538/dexter/apk/Apk.java
@@ -1,8 +1,10 @@
 package uk.ac.cam.db538.dexter.apk;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import lombok.Getter;
@@ -24,7 +26,7 @@ public class Apk {
 
   public Apk(File filename, File androidJar) throws IOException {
     this.dexFile = new Dex(filename, androidJar);
-    this.temporaryFilename = File.createTempFile("dexter-", "-tempAPK");
+    this.temporaryFilename = File.createTempFile("dexter-", ".apk");
     FileUtils.copyFile(filename, this.temporaryFilename);
     try {
       this.originalFile = new ZipFile(this.temporaryFilename);
@@ -62,8 +64,63 @@ public class Apk {
     }
 
     // generate a key
+    val keyFile = File.createTempFile("dexter-", ".keystore");
+    keyFile.delete();
+    val keyToolPB = new ProcessBuilder("keytool", 
+    		"-genkey", 
+    		"-keystore", keyFile.getAbsolutePath(),
+    	    "-storepass", "dexter", 
+    	    "-alias", "DexterKey", 
+    	    "-keypass", "dexter", 
+    	    "-keyalg", "RSA", 
+    	    "-keysize", "2048",
+    	    "-dname", "CN=Android Debug,O=Android,C=US", 
+    	    "-validity", "10000");
+    keyToolPB.redirectErrorStream(true);
+    val keyToolProcess = keyToolPB.start();
+    
+	val keyToolExecOutputReader = new BufferedReader(new InputStreamReader(keyToolProcess.getInputStream()));
+	String keyToolExecOutputLine;
+	while ((keyToolExecOutputLine = keyToolExecOutputReader.readLine()) != null)
+		System.err.println(keyToolExecOutputLine);
+	
+    int keyToolExecResult;
+	try {
+		keyToolExecResult = keyToolProcess.waitFor();
+	} catch (InterruptedException e) {
+		keyToolExecResult = -99;
+	}
+    if (keyToolExecResult != 0) {
+    	throw new IOException("Keytool execution failed (code " + keyToolExecResult + ")");
+    }
 
-
+    // sign the file
+    val jarsignerPB = new ProcessBuilder("jarsigner",
+    		"-keystore", keyFile.getAbsolutePath(),
+    	    "-storepass", "dexter", 
+    	    "-keypass", "dexter", 
+    	    "-sigalg", "MD5withRSA", 
+    	    "-digestalg", "SHA1",
+    	    temporaryFilename.getAbsolutePath(), 
+    	    "DexterKey"); 
+      jarsignerPB.redirectErrorStream(true);
+    val jarsignerProcess = jarsignerPB.start();
+    
+	val jarsignerExecOutputReader = new BufferedReader(new InputStreamReader(jarsignerProcess.getInputStream()));
+	String jarsignerExecOutputLine;
+	while ((jarsignerExecOutputLine = jarsignerExecOutputReader.readLine()) != null)
+		System.err.println(jarsignerExecOutputLine);
+	
+    int jarsignerExecResult;
+	try {
+		jarsignerExecResult = jarsignerProcess.waitFor();
+	} catch (InterruptedException e) {
+		jarsignerExecResult = -99;
+	}
+    if (jarsignerExecResult != 0) {
+    	throw new IOException("Jarsigner execution failed (code " + jarsignerExecResult + ")");
+    }
+    
     // copy the temp file to the given location
     FileUtils.copyFile(temporaryFilename, filename);
   }
