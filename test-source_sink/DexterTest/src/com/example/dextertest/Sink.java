d045c60669b2ac672841acf5b7d9fa641b6b3436
David Brazdil <david.brazdil@gmail.com>
2013-02-05 18:58:33 +0000

    Source/Sink testing app added

---
+++ test-source_sink/DexterTest/src/com/example/dextertest/Sink.java
@@ -0,0 +1,5 @@
+package com.example.dextertest;
+
+public interface Sink {
+  void sendData(String data);
+}
---
+++ test-source_sink/DexterTest/src/com/example/dextertest/Source.java
@@ -0,0 +1,7 @@
+package com.example.dextertest;
+
+import android.content.Context;
+
+public interface Source {
+  public String getData(Context context);
+}
