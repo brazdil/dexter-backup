55b04454b7185fc61aa98ff3561ba579d55d4fc7
David Brazdil <david.brazdil@gmail.com>
2013-02-22 13:54:22 +0000

    Files instrumented and saved from console

---
+++ run_console.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp build/jar/dexter.jar:"lib/*" uk.ac.cam.db538.dexter.MainConsole $@
--- src/uk/ac/cam/db538/dexter/MainConsole.java
+++ src/uk/ac/cam/db538/dexter/MainConsole.java
@@ -1,8 +1,35 @@
 package uk.ac.cam.db538.dexter;
 
+import uk.ac.cam.db538.dexter.apk.Apk;
+
+import java.io.File;
+import java.io.IOException;
+import lombok.val;
+
 public class MainConsole {
-    public static void main(String[] args) {
-        for (String arg : args)
-            System.out.println(arg);
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("usage: dexter <framework-dir> <apk-file>");
+            System.exit(1);
+        }
+
+        val apkFile = new File(args[1]);
+        if (!apkFile.isFile()) {
+            System.err.println("<apk-file> is not a file");
+            System.exit(1);
+        }
+        val apkFile_new = new File(args[1] + ".dexter");
+
+        val frameworkDir = new File(args[0]);
+        if (!frameworkDir.isDirectory()) {
+            System.err.println("<framework-dir> is not a directory");
+            System.exit(1);
+        }
+            
+        val APK = new Apk(apkFile, frameworkDir);
+        val warnings = APK.getDexFile().instrument();
+        for (val warning : warnings)
+            System.err.println("warning: " + warning.getMessage());
+        APK.writeToFile(apkFile_new);
     }
 }
