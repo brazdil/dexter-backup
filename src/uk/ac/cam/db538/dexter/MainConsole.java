da17a78fe69c688fccedd873f2a6b4add898473c
David Brazdil <david.brazdil@gmail.com>
2013-02-22 13:25:21 +0000

    Created MainConsole class and running script

---
+++ src/uk/ac/cam/db538/dexter/MainConsole.java
@@ -0,0 +1,8 @@
+package uk.ac.cam.db538.dexter;
+
+public class MainConsole {
+    public static void main(String[] args) {
+        for (String arg : args)
+            System.out.println(arg);
+    }
+}
