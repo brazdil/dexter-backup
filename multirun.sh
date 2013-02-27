e8e9951bfab058e9d300f3a3d48e7243a91dae75
David Brazdil <david.brazdil@gmail.com>
2013-02-27 12:14:20 +0000

    Multirun script swaps original/instrumented

--- multirun.sh
+++ multirun.sh
@@ -2,7 +2,8 @@
 N="$1"
 PACKAGE="$2"
 ACTIVITY="$3"
-APK="$4"
+APK_ORIGINAL="$4"
+APK_INSTRUMENTED="$APK_ORIGINAL.dexter"
 
 for i in $(seq 1 $N)
 do
@@ -11,8 +12,26 @@ do
 	echo " - uninstalling the package"
 	adb uninstall $PACKAGE > /dev/null
 
-	echo " - installing the APK"
-	ADB_OUTPUT=`adb install "$APK" 2>/dev/null | tail -n 1`
+	echo " - installing the original APK"
+	ADB_OUTPUT=`adb install "$APK_ORIGINAL" 2>/dev/null | tail -n 1`
+	if [[ "$ADB_OUTPUT" != Success* ]]
+	then
+		echo "$ADB_OUTPUT"
+		echo "ERROR: couldn't install APK"
+		exit;
+	fi
+
+	echo " - launching the application"
+	adb shell am start -n "$PACKAGE"/."$ACTIVITY"
+
+	echo " - please press ENTER"
+	read X
+
+	echo " - uninstalling the package"
+	adb uninstall $PACKAGE > /dev/null
+
+	echo " - installing the instrumented APK"
+	ADB_OUTPUT=`adb install "$APK_INSTRUMENTED" 2>/dev/null | tail -n 1`
 	if [[ "$ADB_OUTPUT" != Success* ]]
 	then
 		echo "$ADB_OUTPUT"
