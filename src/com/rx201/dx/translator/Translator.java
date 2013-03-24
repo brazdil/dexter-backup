b5261768b877c0a144e10414aaededcd51ddc98a
Rubin Xu <Rubin.Xu@cl.cam.ac.uk>
2013-03-24 23:59:57 +0000

    Android SDK's dx source incorperated.

---
+++ src/com/rx201/dx/translator/Translator.java
@@ -0,0 +1,14 @@
+package com.rx201.dx.translator;
+
+import org.jf.dexlib.CodeItem;
+
+import com.android.dx.rop.code.RopMethod;
+
+public class Translator {
+
+	public static RopMethod toRop(CodeItem method) {
+		return null;
+		
+	}
+
+}
