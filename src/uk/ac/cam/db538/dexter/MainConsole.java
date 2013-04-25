c811230fa6917e8c28015bdf5d5d40302692551a
David Brazdil <david.brazdil@gmail.com>
2013-04-25 14:46:04 +0100

    Running from console instruments again

--- src/uk/ac/cam/db538/dexter/MainConsole.java
+++ src/uk/ac/cam/db538/dexter/MainConsole.java
@@ -27,11 +27,13 @@ public class MainConsole {
     }
 
     val APK = new Apk(apkFile, frameworkDir);
-//    val warnings = APK.getDexFile().instrument(false);
-//    for (val warning : warnings)
-//      System.err.println("warning: " + warning.getMessage());
-//    // APK.getDexFile().transformSSA();
-//    APK.writeToFile(apkFile_new);
+    APK.getDexFile().countInstructions();
+    val warnings = APK.getDexFile().instrument(false);
+    for (val warning : warnings)
+      System.err.println("warning: " + warning.getMessage());
+    // APK.getDexFile().transformSSA();
+    APK.getDexFile().countInstructions();
+    APK.writeToFile(apkFile_new);
     APK.getDexFile().countInstructions();
   }
 }
