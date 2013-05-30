14599091419b1b981a284cb1f66e978d611a5160
Rubin Xu <Rubin.Xu@cl.cam.ac.uk>
2013-05-31 00:19:31 +0100

    tweak long int test case.

--- cg_test/wide/TestClass.java
+++ cg_test/wide/TestClass.java
@@ -1,7 +1,7 @@
 public class TestClass {
 
 	private static long change(long x, long y) {
-		return (2 * x  + 3 * y) / ((int)y - (int)x);
+		return (2L * x  + 3L * y) / ((int)y - (int)x);
 	}
 	public static void main(String[] arg) {
 		System.out.println(change(1, 2));
