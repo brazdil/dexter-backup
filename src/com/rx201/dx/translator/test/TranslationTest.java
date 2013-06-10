4a189e22acd27bbd82a9dd6e6032e4616944a3bd
Rubin Xu <xurubin@gmail.com>
2013-06-10 19:11:02 +0100

    parameterized unit tests.

--- src/com/rx201/dx/translator/test/TranslationTest.java
+++ src/com/rx201/dx/translator/test/TranslationTest.java
@@ -1,19 +1,23 @@
 package com.rx201.dx.translator.test;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import lombok.val;
 
 import org.jf.dexlib.Code.Analysis.ClassPath;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import uk.ac.cam.db538.dexter.apk.Apk;
 
+@RunWith(Parameterized.class)
 public class TranslationTest {
 
 	private static File frameworkDir = new File("framework-2.3-lite");
@@ -33,20 +37,30 @@ public class TranslationTest {
 	    		false);
     }
 	
-	private void testEach(File file) throws IOException {
-	    val APK = new Apk(file, frameworkDir);
-	    APK.getDexFile().writeToFile();
-	}
-	
-	@Test
-	public void test() throws IOException {
-	    
+	@Parameters(name = "{1}")
+	public static Collection<Object[]> data() {
+		ArrayList<Object[]> tests = new ArrayList<Object[]>();
+		
 		for (String file : testsDir.list(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 				return name.endsWith(".apk");
 			}
 		})) {
-			testEach(new File(testsDir.getAbsolutePath(), file ));
+			tests.add(new Object[] {new File(testsDir.getAbsolutePath(), file ), file});
 		}
+		return tests;
 	}
+	
+	private File file;
+	
+	public TranslationTest(File file, String filename) {
+		this.file = file;
+	}
+	
+	@Test
+	public void test() throws IOException{
+	    val APK = new Apk(file, frameworkDir);
+	    APK.getDexFile().writeToFile();
+	}
+	
 }
