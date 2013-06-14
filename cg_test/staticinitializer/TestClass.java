3fb196a71b91d3d7c997002821de4c37b8796156
Rubin Xu <xurubin@gmail.com>
2013-06-14 20:01:28 +0100

    static initializer test.

---
+++ cg_test/staticinitializer/TestClass.java
@@ -0,0 +1,20 @@
+class TestStatic {
+	final static int staticInt = 1;
+	final static long[] staticArray = {1,2,3,4};
+	final static double staticDouble = 3.14159;
+	final static String staticString = "A string";
+	final static Object staticZObject = null;
+}
+
+public class TestClass {
+	
+	public static void main(String[] arg) {
+	/*
+		System.out.println(TestStatic.staticInt);
+		System.out.println(TestStatic.staticDouble);
+		System.out.println(TestStatic.staticString);
+		for(int i=0; i<TestStatic.staticArray.length; i++) 
+			System.out.println(TestStatic.staticArray[i]);
+	*/
+	}
+}
