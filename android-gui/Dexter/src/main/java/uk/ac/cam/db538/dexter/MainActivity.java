cd6350a77b5227b5b98abbeb25b793a177ca241b
David Brazdil <david.brazdil@gmail.com>
2013-07-02 17:18:47 +0100

    Shows a list of packages

--- android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/MainActivity.java
+++ android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/MainActivity.java
@@ -1,23 +1,37 @@
 package uk.ac.cam.db538.dexter;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    protected ListView listPackages = null;
+    protected PackageAdapter adapterPackages = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        listPackages = (ListView) this.findViewById(R.id.listPackages);
+        adapterPackages = new PackageAdapter(this);
+        listPackages.setAdapter(adapterPackages);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
