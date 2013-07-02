29929c846113d9cb5944e7ba33024a574984cc0e
David Brazdil <david.brazdil@gmail.com>
2013-07-02 23:03:00 +0100

    Passing package info to detail activity/fragment

--- android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/PackageDetailFragment.java
+++ android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/PackageDetailFragment.java
@@ -1,13 +1,13 @@
 package uk.ac.cam.db538.dexter;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import uk.ac.cam.db538.dexter.dummy.DummyContent;
 
 /**
  * A fragment representing a single Package detail screen.
@@ -16,46 +16,41 @@ import uk.ac.cam.db538.dexter.dummy.DummyContent;
  * on handsets.
  */
 public class PackageDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
-    public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
+    private PackageInfo packageInfo;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public PackageDetailFragment() {
-    }
+    public PackageDetailFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            String packageName = getArguments().getString(ARG_ITEM_ID);
+            try {
+                packageInfo = this.getActivity().getPackageManager().getPackageInfo(packageName, 0);
+            } catch (PackageManager.NameNotFoundException ex) {
+                // package of given name not found
+                // TODO: show error message
+                throw new RuntimeException(ex);
+            }
+        } else {
+            // intent does not contain package name
+            // TODO: show error message
+            throw new RuntimeException();
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_package_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.package_detail)).setText(mItem.content);
+        if (packageInfo != null) {
+            ((TextView) rootView.findViewById(R.id.package_detail)).setText(packageInfo.packageName);
         }
 
         return rootView;
     }
+
+    public static final String ARG_ITEM_ID = "package_name";
 }
