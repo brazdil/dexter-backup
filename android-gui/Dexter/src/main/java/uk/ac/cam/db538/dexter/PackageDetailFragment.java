1cc319e0ead3e6b308d9f69a2966f229eb9c7fb3
David Brazdil <david.brazdil@gmail.com>
2013-07-03 00:06:22 +0100

    More info about the package

--- android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/PackageDetailFragment.java
+++ android-gui/Dexter/src/main/java/uk/ac/cam/db538/dexter/PackageDetailFragment.java
@@ -2,12 +2,19 @@ package uk.ac.cam.db538.dexter;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * A fragment representing a single Package detail screen.
@@ -17,7 +24,17 @@ import android.widget.TextView;
  */
 public class PackageDetailFragment extends Fragment {
 
+    private PackageManager packageManager;
+
     private PackageInfo packageInfo;
+    private File packageFile;
+
+    private ImageView imgPackageIcon;
+    private TextView textPackageName;
+    private EditText textPackageVersion;
+    private EditText textLastUpdated;
+    private EditText textApkPath;
+    private EditText textApkSize;
 
     public PackageDetailFragment() { }
 
@@ -25,10 +42,21 @@ public class PackageDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        packageManager = this.getActivity().getPackageManager();
+
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             String packageName = getArguments().getString(ARG_ITEM_ID);
             try {
-                packageInfo = this.getActivity().getPackageManager().getPackageInfo(packageName, 0);
+                packageInfo = packageManager.getPackageInfo(packageName, 0);
+                packageFile = new File(packageInfo.applicationInfo.sourceDir);
+
+                if (!packageFile.exists()) {
+                    // TODO: show error message
+                    throw new RuntimeException();
+                } else if (!packageFile.canRead()) {
+                    // TODO: show error message
+                    throw new RuntimeException();
+                }
             } catch (PackageManager.NameNotFoundException ex) {
                 // package of given name not found
                 // TODO: show error message
@@ -45,12 +73,30 @@ public class PackageDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_package_detail, container, false);
 
+        imgPackageIcon = (ImageView) rootView.findViewById(R.id.imgPackageIcon);
+        textPackageName = (TextView) rootView.findViewById(R.id.textPackageName);
+        textPackageVersion = (EditText) rootView.findViewById(R.id.textVersion);
+        textLastUpdated = (EditText) rootView.findViewById(R.id.textLastUpdated);
+        textApkPath = (EditText) rootView.findViewById(R.id.textApkPath);
+        textApkSize = (EditText) rootView.findViewById(R.id.textApkSize);
+
         if (packageInfo != null) {
-            ((TextView) rootView.findViewById(R.id.package_detail)).setText(packageInfo.packageName);
+            Drawable icon = packageInfo.applicationInfo.loadIcon(packageManager);
+            DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
+            String lastUpdated = df.format(new Date(packageInfo.lastUpdateTime));
+            String apkSize = Long.toString(packageFile.length() / 1024) + " KB";
+
+            imgPackageIcon.setImageDrawable(icon);
+            textPackageName.setText(packageInfo.packageName);
+            textPackageVersion.setText(packageInfo.versionName);
+            textLastUpdated.setText(lastUpdated);
+            textApkPath.setText(packageInfo.applicationInfo.sourceDir);
+            textApkSize.setText(apkSize);
         }
 
         return rootView;
     }
 
     public static final String ARG_ITEM_ID = "package_name";
+
 }
