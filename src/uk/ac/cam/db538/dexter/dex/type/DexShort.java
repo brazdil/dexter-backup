45d7214d7bc326d8e02393e2c0e62f9e05c649cd
David Brazdil <david.brazdil@gmail.com>
2013-07-04 12:52:25 +0100

    Types renamed back (take 2)

---
+++ src/uk/ac/cam/db538/dexter/dex/type/DexShort.java
@@ -0,0 +1,34 @@
+package uk.ac.cam.db538.dexter.dex.type;
+
+import uk.ac.cam.db538.dexter.utils.Pair;
+
+public class DexShort extends DexPrimitiveType {
+    DexShort() { }
+
+    @Override
+	public DexRegisterTypeSize getTypeSize() {
+    	return DexRegisterTypeSize.SINGLE;
+	}
+
+	@Override
+	public String getDescriptor() {
+		return "S";
+	}
+
+	@Override
+	public String getPrettyName() {
+		return "short";
+	}
+    
+    @Override
+    public Pair<DexClassType, String> getPrimitiveClassConstantField(DexTypeCache cache) {
+      return new Pair<DexClassType, String>(DexClassType.parse("Ljava/lang/Short;", cache), "TYPE");
+    }
+
+    public static DexShort parse(String typeDescriptor, DexTypeCache cache) {
+    	if (!typeDescriptor.equals("S"))
+    		throw new UnknownTypeException(typeDescriptor);
+    	else
+    		return cache.getCachedType_Short();
+    }
+}
